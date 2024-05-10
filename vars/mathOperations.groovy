2e884dcb9868e6cdd26797224fbcb1e27df27f66
Harini-S21 <harinisambath21@gmail.com>
2024-05-10 11:26:47 +0530

    1st commit

--- vars/mathOperations.groovy
+++ vars/mathOperations.groovy
@@ -1,18 +1,17 @@
-def call(int num1, int num2) {
-    return num1 + num2
-}
-
-def subtract(int num1, int num2){
-    return num1 - num2
-}
-
-def multiply(int num1, int num2) {
-    return num1 * num2
-}
-
-def divide(int num1, int num2) {
-    if (num2 == 0) {
-        throw new IllegalArgumentException("Division by zero is not allowed")
+def call(String operation, int num1, int num2) {
+    switch (operation) {
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if (num2 == 0) {
+                throw new IllegalArgumentException("Division by zero is not allowed")
+            }
+            return num1 / num2
+        default:
+            throw new IllegalArgumentException("Unsupported operation: $operation")
     }
-    return num1 / num2
 }
