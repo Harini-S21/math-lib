dbd261757c2e58e4499f947889743950cdb6381a
Harini-S21 <harinisambath21@gmail.com>
2024-05-10 10:53:25 +0530

    1st commit

---
+++ vars/mathOperations.groovy
@@ -0,0 +1,18 @@
+def add(int num1, int num2) {
+    return num1 + num2
+}
+
+def subtract(int num1, int num2) {
+    return num1 - num2
+}
+
+def multiply(int num1, int num2) {
+    return num1 * num2
+}
+
+def divide(int num1, int num2) {
+    if (num2 == 0) {
+        throw new IllegalArgumentException("Division by zero is not allowed")
+    }
+    return num1 / num2
+}
